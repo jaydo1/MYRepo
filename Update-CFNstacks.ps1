9d25988e73adbefcf52537d92c97ab8a1f2a1e97
jaydo1 <fatalrecords@hotmail.com>
2017-02-23 13:19:37 +1100

    update file
    new files

---
+++ Update-CFNstacks.ps1
@@ -0,0 +1,8 @@
+﻿$StackName = Get-Content C:\StackName.txt
+$intanceSize = "r3.xlarge"
+
+Foreach($Stack in $StackName){  
+
+Update-CFNStack -StackName $Stack -UsePreviousTemplate $true  -Parameter @( @{ parameterKey="InstanceType";ParameterValue="$intanceSize"})`
+-Capabilities "CAPABILITY_IAM"
+}
---
+++ find-groupsloggedonusers.ps1
@@ -0,0 +1,14 @@
+﻿#requires -Version 1 
+try 
+{  
+  $groups = ([Security.Principal.WindowsIdentity]::GetCurrent()).Groups |
+  ForEach-Object {
+    $_.Translate([Security.Principal.NTAccount])
+  } | Sort-Object
+} 
+catch 
+{ 
+  Write-Warning 'Groups could not be retrieved.'
+}
+ 
+$groups 
